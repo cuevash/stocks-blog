9d322dd83e3e4dcc74d7ca6b56115fc7a4d049cf
Hector Cuevas <cuevash.sp@gmail.com>
2022-04-26 23:19:31 +0200

    small change

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,8 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+// Main APP
+
 export default function App() {
   return (
     <html lang="en">
